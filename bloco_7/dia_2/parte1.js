566abc914bb3fceb9b8ebf5eb3065d25680f0d98
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-25 23:45:11 -0300

    Exercicios refatorados

--- bloco_7/dia_2/parte1.js
+++ bloco_7/dia_2/parte1.js
@@ -34,26 +34,26 @@ const order = {
     },
   };
   
-  const customerInfo = (order) => {
-    console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone ${order.phoneNumber}, R. ${order.address.street} Nº: ${order.address.number}, AP: ${order.address.apartment}.`)
-  }
-  customerInfo(order);
+  // const customerInfo = (order) => {
+  //   console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone ${order.phoneNumber}, R. ${order.address.street} Nº: ${order.address.number}, AP: ${order.address.apartment}.`)
+  // }
+  // customerInfo(order);
 
 
 const orderModifier = (order) => {
     //adicionando um nome
-    const client = order.name = 'Luiz Silva';
+    order.name = 'Luiz Silva';
     order.order.flavor = {};
     //adicionando um objeto dentro de outro objeto
-    order.order.flavor['mussarela'] = {amount: 1, price: 25};
-    order.order.flavor['calabresa'] = {amount: 1, price: 25};
+    order.order.pizza['mussarela'] = {amount: 1, price: 25};
+    order.order.pizza['calabresa'] = {amount: 1, price: 25};
     //acessando as chaves do objeto criado
     const pizzas = Object.keys(order.order.flavor);
     //acessando a bebida
     const soda = order.order.drinks.coke.type; 
     //atualizando o valor do pedido
     const total = order.payment.total = '60';
-    console.log(`Olá ${client}, o total do seu pedido de ${pizzas} e ${soda} é R$ ${total},00.`);
+    console.log(`Olá ${order.name}, o total do seu pedido de ${pizzas} e ${soda} é R$ ${total},00.`);
   }
   orderModifier(order);
  
