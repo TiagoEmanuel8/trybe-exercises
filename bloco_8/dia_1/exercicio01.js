140b9805ceea2c53ad71ddb943ef6a43f868fcfc
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-12-01 12:06:24 -0300

    Exercicio 1 finalizado

---
+++ bloco_8/dia_1/exercicio01.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+function authorBornIn1947() {
+    //usando o método antigo
+    for (let key in books) {
+        if (books[key].author.birthYear === 1947){
+            // console.log(books[key].author.birthYear);
+            return books[key].author.name;
+        }
+    }
+
+    // usando a aula de hoje
+    const findAuthor = books.find((find) => find.author.birthYear === 1947);
+    return findAuthor.author.name;
+};
+
+assert.equal(authorBornIn1947(), 'Stephen King');
