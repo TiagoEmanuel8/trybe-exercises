7c4e74ad796fa3d3a0e2af4bf356ca6ba5e77428
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-12-03 11:32:26 -0300

    Exercicio 5 - Finalizado

--- bloco_8/dia_2/exercicio5.js
+++ bloco_8/dia_2/exercicio5.js
@@ -64,33 +64,15 @@ const books = [
 ];
 
 const expectedResult = [
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
-  }
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien'
 ]
 
-function oldBooks() {
+function fantasyOrScienceFictionAuthors() {
   // escreva seu código aqui
-  return books.filter((year) => year.releaseYear < 2020 - 60)
-  .sort((yearA, yearB) => yearA.releaseYear - yearB.releaseYear);
+  return books.filter((genreBook) => genreBook.genre === 'Ficção Científica' || genreBook.genre === 'Fantasia').map((filterGenre) => filterGenre.author.name).sort()
 }
 
-assert.deepStrictEqual(oldBooks(), expectedResult);
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
