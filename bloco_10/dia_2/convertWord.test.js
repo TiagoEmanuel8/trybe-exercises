4b83cc07ae5c7a2afa09b3ac1b5e74aa52147207
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-01-08 19:01:02 -0300

    Testes para a funçao uppercase concluidos

--- bloco_10/dia_2/convertWord.test.js
+++ bloco_10/dia_2/convertWord.test.js
@@ -1,1 +1,10 @@
-const uppercase = require('./convertWord.js')
+const uppercase = require('./convertWord.js');
+
+describe('testando a função uppercase', () => {
+    it('Teste para saber se a função está convertendo a string de minúscula para maiúscula', (done) => {
+        uppercase('pizza', (funcao) => {
+            expect(funcao).toBe('PIZZA');
+            done();
+        });
+    });
+});
