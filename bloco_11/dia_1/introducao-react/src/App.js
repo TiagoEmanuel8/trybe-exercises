989a5ffe002bfb2bd8c1a7b5fee9965743f0a600
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-01-12 22:21:03 -0300

    Criando uma lista ul atraves de HOF em react

--- bloco_11/dia_1/introducao-react/src/App.js
+++ bloco_11/dia_1/introducao-react/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+
 import './App.css';
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+};
+const compromissos = ["Acordar", "Tomar café", "Escovar os dentes", "Ir trabalhar"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //fazendo direto
+    <ul>{ compromissos.map((item) => <li> {item} </li>)}</ul>
+
+    //acionando a callback
+    // compromissos.map((item) => task(item))
   );
 }
 
