930f52c8a6d172ce305906c988078c0c69ee78e0
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-02-10 15:28:50 -0300

    Função e testes da função randomNumber implementados

--- bloco_10/dia_3/randomNumber.test.js
+++ bloco_10/dia_3/randomNumber.test.js
@@ -1,1 +1,18 @@
-const service = require('./randomNumber.js');
+const randomNumber = require('./randomNumber.js');
+
+describe('Testando a função randomNumber', () => {
+    test('Testando se a função é chamada ', () => {
+        const randomNumber = jest.fn()
+        randomNumber()
+        expect(randomNumber).toHaveBeenCalled();
+    });
+    test('Testando o retorno da função fazendo simulação', () => {
+        const randomNumber = jest.fn().mockReturnValue(10)
+        expect(randomNumber()).toBe(10)
+    });
+    test('Testando quantas vezes a função foi chamada', () => {
+        const randomNumber = jest.fn()
+        randomNumber()
+        expect(randomNumber).toHaveBeenCalledTimes(1)
+    });
+});
