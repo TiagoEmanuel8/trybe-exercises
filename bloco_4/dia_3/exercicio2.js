39926526565ac6622a014f7b8f13ed8fc251a5a9
Tiago Emanuel <tiago.emanuel.n@gmail.com>
2021-05-14 12:33:01 -0300

    Revert "Fundamentos do desenvolvimento web"

---
+++ bloco_4/dia_3/exercicio2.js
@@ -0,0 +1,8 @@
+let n = 5;
+let asterisk = "*";
+let line = ' ';
+
+for (let index = 0; index <= n; index += 1 ) {
+     console.log(line);
+     line += asterisk;
+};
---
+++ bloco_4/dia_4/exercicio10.js
@@ -0,0 +1,18 @@
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero++;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+  return numeros[indexNumeroRepetido];
