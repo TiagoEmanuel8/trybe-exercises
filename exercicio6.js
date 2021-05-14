757782894633d2ae52860691d2e21870fa3074e3
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-05-14 18:38:46 -0300

    [bloco 7 - dia 1] - Reorganizando as pastas

--- exercicio6.js
+++
@@ -1,9 +0,0 @@
-//Esse exercício era para substituir uma parte da palavra por outra
-//o método "string.replace('palavraAntiga', 'palavraNova')"" resolve esse problema
-const replace = (str) => {
-    let phrase = 'Trybe x aqui';
-    let newPhrase = phrase.replace('x', str);
-    return newPhrase;
-}
-console.log(replace('Tiago'))
-
