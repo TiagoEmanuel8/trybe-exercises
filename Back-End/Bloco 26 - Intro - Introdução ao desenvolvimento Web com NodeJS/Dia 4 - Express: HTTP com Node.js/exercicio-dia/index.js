0413e74672239e2fc778f35a2cc0d35754909069
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-06-25 20:47:50 -0300

    [26.4] - Exercicios Parcialmente Finalizados

---
+++ Back-End/Bloco 26 - Intro - Introdução ao desenvolvimento Web com NodeJS/Dia 4 - Express: HTTP com Node.js/exercicio-dia/index.js
@@ -0,0 +1,123 @@
+// Aprendi esse método de organizar com o Tiago Yoneda, obrigado !!!
+
+// Estrutura Básica
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// app.listen(3001, () => {
+//   console.log('Porta 3001, Ok!!!');
+// });
+
+// -----------------------------------------------
+
+// Exercicio 1 - 
+// Crie uma rota GET /ping
+// Sua rota deve retornar o seguinte JSON: { message: 'pong' }
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// // 1.1 - Criei uma função que retorna uma mensagem
+// const message = (_req, res) => {
+//   res.json({ message: 'pong' });
+// };
+// // 1.2 => Criei a rota /ping
+// // 1.3 => chamada => http GET :3001/ping
+// app.get('/ping', message);
+
+// app.listen(3001, () => {
+//   console.log('Aplicação ouvindo na porta 3001');
+// });
+
+//---------------------------------------------
+
+// Exercicio 2 -
+// Crie uma rota POST /hello
+// Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+// Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// // Exercicio 2 - Crie uma rota POST /hello 
+// // 2.1 => função para capturar o que for passado após o '='
+// const username = (req, res) => {
+//   res.status(200).json({ message: `Hello, ${req.body.name}` });
+//   next();
+// }; // => sintaxe linda, obrigado yoneda
+
+// // 2.2 => Criação da rota /hello + função mensagem
+// // 2.3 => chamada => http POST :3001/hello name=nome
+// // Obs: usar o '=' para chamar no corpo
+// app.post('/hello', username);
+
+
+// app.listen(3001, () => {
+//   console.log('Aplicação ouvindo na porta 3001');
+// });
+
+//----------------------------------------------------
+
+// Exercicio 3 - 
+// Crie uma rota POST /greetings
+// Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> } .
+// Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK .
+// Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized .
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// const userAge = (req, res) => {
+//   const name = req.body.name;
+//   const ageCheck = parseInt(req.body.age);
+
+//   if(ageCheck <= 17) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+//   res.status(200).json({
+//     message: `Hello ${name}`
+//   });
+// };
+
+// app.post('/greetings', userAge);
+// app.listen(3001, () => {
+//   console.log('Aplicação ouvindo na porta 3001');
+// });
+
+//---------------------------------------------
+
+// Exercicio 4 -
+// Crie uma rota PUT /users/:name/:age .
+// Sua rota deve retornar o seguinte JSON: { "message": "Seu nome é <name> e você tem <age> anos de idade" }.
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+// 4.1 - Função que trata a url
+const user = (req, res) => {
+  const { name, age } = req.params; // Pega partes da url
+  res.status(200).json({
+    message: `Seu nome é ${name} e você tem ${age} anos de idade`
+  });
+};
+// 4.2 - Definindo a rota do exercicio
+// chamada => http PUT :3001/users/Tiago/20
+app.put('/users/:name/:age', user);
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
--- Back-End/Bloco 26 - Intro - Introdução ao desenvolvimento Web com NodeJS/Dia 4 - Express: HTTP com Node.js/hello-express/index.js
+++ Back-End/Bloco 26 - Intro - Introdução ao desenvolvimento Web com NodeJS/Dia 4 - Express: HTTP com Node.js/hello-express/index.js
@@ -1,23 +1,28 @@
-// // 1 - Importar o NodeJs
-// // const express = require('express');
+// 1 - Importar o NodeJs
+const express = require('express');
 
-// // // 2 - Instanciar o express
-// // const app = express(); // 1
+// // 2 - Instanciar o express
+const app = express(); // 1
 
-// // // 4 - Get vai dizer quando tiver essa rota, vai chamar a função
-// // app.get('/hello', handleHelloWorldRequest); // 2
+// 4 - Get vai dizer quando tiver essa rota, vai chamar a função
+app.get('/hello', function (req, res){
+  res.send('Teste');
+}); // 2
 
-// // // 3 - Pedir para escutar a porta 3000 e mostrar um console.log()
-// // app.listen(3000, () => {
-// //   console.log('Aplicação ouvindo na porta 3000');
-// // }); // 3
+// 3 - Pedir para escutar a porta 3000 e mostrar um console.log()
+app.listen(3000, () => {
+   console.log('Aplicação ouvindo na porta 3000');
+}); // 3
 
-// // function handleHelloWorldRequest(req, res) {
-// //   res.status(200).send('Hello World!'); // 4
-// // }
+// function handleHelloWorldRequest(req, res) {
+//    res.status(200).send('Hello World!'); // 4
+// }
+// -----------------------------------------------
 
-// // 1 - importar o módulo responsável
-// // const bodyParser = require('body-parser');
+// 1 - importar o módulo responsável
+// const bodyParser = require('body-parser');
+
+
 
 // // const express = require('express');
 // // const app = express();
@@ -41,37 +46,39 @@
 
 // // app.listen(3000, () => console.log('App rodando na porta 3000!'));
 
+//---------------------------------------------------------
+
 // Middleware
-const bodyParser = require('body-parser'); 
-const express = require('express');
-const app = express();
+// const bodyParser = require('body-parser'); 
+// const express = require('express');
+// const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
  
-app.get('/', (_req, res) => {
- res.send('Hello World');
-});
+// app.get('/', (_req, res) => {
+//  res.send('Hello World');
+// });
  
-const langs = [ 'HTML', 'CSS', 'JS', 'React' ];
+// const langs = [ 'HTML', 'CSS', 'JS', 'React' ];
 
-const logMiddleware = (req, res, next) => {
-  const { token } = req.headers;
-  if(!token) return res.send('Token não enviado');
-  next();
-}
-// Esse use vai ser chamado antes de qualquer método rodando abaixo dele
-app.use((req, res, next) => {
-  console.log('Passou por aqui');
-  next();
-})
+// const logMiddleware = (req, res, next) => {
+//   const { token } = req.headers;
+//   if(!token) return res.send('Token não enviado');
+//   next();
+// }
+// // Esse use vai ser chamado antes de qualquer método rodando abaixo dele
+// app.use((req, res, next) => {
+//   console.log('Passou por aqui');
+//   next();
+// })
 
-app.get('/langs', logMiddleware, (req, res) => { res.send(langs) });
+// app.get('/langs', logMiddleware, (req, res) => { res.send(langs) });
 
-app.post('/langs', logMiddleware, (req, res) => {
- const { name } = req.body;
- langs.push(name);
- res.send(`Linguagem ${name} adicionada com sucesso!`);
-});
+// app.post('/langs', logMiddleware, (req, res) => {
+//  const { name } = req.body;
+//  langs.push(name);
+//  res.send(`Linguagem ${name} adicionada com sucesso!`);
+// });
  
-app.listen(3000, () => console.log('App rodando na porta 3000!'));
+// app.listen(3000, () => console.log('App rodando na porta 3000!'));
 
