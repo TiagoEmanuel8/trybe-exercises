aa30dbc1c41c7a54fedc9d2382b445f8601b5983
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-07-05 17:09:46 -0300

    [12.1] - Exercicios Finalizados

--- Front-End/Bloco 12 - Componentes com Estado, Eventos e Formulários com React/Dia 1 - Components com estado e evento/exercise-pokedex-state/src/Pokedex.js
+++ Front-End/Bloco 12 - Componentes com Estado, Eventos e Formulários com React/Dia 1 - Components com estado e evento/exercise-pokedex-state/src/Pokedex.js
@@ -2,11 +2,30 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { selectedPokemon: 0 }
+        this.nextPokemon = this.nextPokemon.bind(this);
+    };
+
+    nextPokemon(selectedPokemon) {
+        const pokemonsProps = this.props.pokemons;
+
+        (selectedPokemon < pokemonsProps.length - 1) ?
+        this.setState((param) => ({selectedPokemon: param.selectedPokemon + 1})) :
+        this.setState({ selectedPokemon: 0 });
+    };
+    
     render() {
+        const pokemonsProps = this.props.pokemons;
+        const { selectedPokemon } = this.state;
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
+            <section>
+                <div className="pokedex">
+                    {<Pokemon pokemon={pokemonsProps[selectedPokemon]} />}
+                </div>
+                <button onClick={ () => this.nextPokemon(selectedPokemon) }>Proximo</button>
+            </section>
         );
     }
 }
