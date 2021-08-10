2b14e4e0db5dab39e75ab6bf64591241b56064e2
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-08-10 19:44:09 -0300

    [27.3] Exercicios Finalizados

--- Back-End/Bloco 27 - NodeJS: Camada de Serviço e Arquitetura Rest e Restful/Dia 3 -  Arquitetura de web - Rest e Restful/Exercicio/controllers/productController.js
+++ Back-End/Bloco 27 - NodeJS: Camada de Serviço e Arquitetura Rest e Restful/Dia 3 -  Arquitetura de web - Rest e Restful/Exercicio/controllers/productController.js
@@ -7,25 +7,25 @@ const getAllProducts = async(_req, res, _next) => {
 
 const getByIdProducts = async (_req, res, _next) => {
   const product = await ProductModel.getById(req.params.id);
-  res.send(product);
+  res.send(200).json(product);
 };
 
 const addUser = async (req, res) => {
   const { name, brand } = req.body;
   const newProduct = await ProductModel.add(name, brand);
-  res.send(newProduct);
+  res.send(200).json(newProduct);
 };
 
 const deleteUser = async(req, res) => {
-    const products = await ProductModel.exclude(req.params.id);
-    res.send(products);
-  };
+  const products = await ProductModel.exclude(req.params.id);
+  res.send(200).json(products);
+};
 
 const updateUser = async (req, res) => {
-    const { name, brand } = req.body;
-    const products = await ProductModel.update(req.params.id, name, brand);
-    res.send(products);
-  };
+  const { name, brand } = req.body;
+  const products = await ProductModel.update(req.params.id, name, brand);
+  res.send(200).json(products);
+};
 
 module.exports = {
   getAllProducts,
