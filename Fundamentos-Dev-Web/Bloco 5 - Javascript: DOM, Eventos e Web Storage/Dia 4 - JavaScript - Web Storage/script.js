fcfb090a474d66fd00941a8c06b3d90c6f7a8e96
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-10-08 11:09:48 -0300

    Bloco 5.4 - Anotaçoes do course

---
+++ Fundamentos-Dev-Web/Bloco 5 - Javascript: DOM, Eventos e Web Storage/Dia 4 - JavaScript - Web Storage/script.js
@@ -0,0 +1,40 @@
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+// grava as frases no local storage
+function addPhraseToLocalStorage() {
+  const oldList = JSON.parse(localStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  localStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+};
+// insere as frases no DOM
+function insertPhraseInDOM() {
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+};
+// controla a renderização das frases do local storage na tela
+function initialRenderization() {
+  if (localStorage.getItem('phrases') === null) {
+    localStorage.setItem('phrases', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      list.appendChild(listElement);
+    };
+  };
+};
+
+button.addEventListener('click', addPhraseToLocalStorage);
+
+window.onload = function() {
+  initialRenderization();
+};
